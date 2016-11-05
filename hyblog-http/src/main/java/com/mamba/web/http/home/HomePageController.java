d7ad7ad5d5e99a0b630a14d7bfcf3b5fafe517c2
cj <cjh_bryant@163.com>
2016-11-05 23:46:49 +0800

    home commit

---
+++ hyblog-http/src/main/java/com/mamba/web/http/home/HomePageController.java
@@ -0,0 +1,22 @@
+package com.mamba.web.http.home;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mamba_cjh on 2016/11/5.
+ */
+@RestController
+@RequestMapping(value = "/api/home")
+public class HomePageController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(HomePageController.class);
+
+    @RequestMapping(value = "/index")
+    public String welcomeHome(){
+        LOGGER.info("============");
+        String test = "welcome...";
+        return test;
+    }
+}
